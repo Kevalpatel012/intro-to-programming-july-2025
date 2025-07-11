50ea60fb5b51c7ed5daa1ecb78bebb49886d739d
Keval Patel <kevalpatel0123@gmail.com>
2025-07-11 18:11:30 +0000

    End of week 1

---
+++ week-1/StringCalculatorKataWeds/StringCalculator/Calculator.cs
@@ -0,0 +1,79 @@
+﻿
+using System.Reflection.Metadata.Ecma335;
+
+public class Calculator
+{
+    public int Add(string numbers)
+    {
+        string[] fruit;
+        string[] veggie;
+
+        int total = 0;
+
+        if (string.IsNullOrEmpty(numbers))
+        {
+            return 0; 
+        }
+
+        if (numbers.Contains(",") && numbers.Contains("\n"))
+        {
+
+            veggie = numbers.Split('\n');
+
+            foreach (string character in veggie)
+            {
+                if (!numbers.Contains("\n"))
+                {
+                    break;
+                }
+
+                total += int.Parse(character);
+                
+            }
+
+            fruit = numbers.Split(',');
+
+            foreach (string num in fruit)
+            {
+                if (!numbers.Contains(","))
+                {
+                    break;
+                }
+
+                total += int.Parse(num);
+                
+            }
+
+            return total;
+        }
+
+        if (numbers.Contains(","))
+        {
+            fruit = numbers.Split(',');
+
+            foreach (string num in fruit)
+            {
+                total += int.Parse(num);
+            }
+
+            return total;
+        }
+        if (numbers.Contains("\n"))
+        {
+            fruit = numbers.Split('\n');
+
+            foreach (string num in fruit)
+            {
+                total += int.Parse(num);
+            }
+
+            return total;
+        }
+        else
+        {
+            return int.Parse(numbers);
+        }
+    }
+
+    
+}
---
+++ week-1/StringCalculatorKataWeds/StringCalculator/CalculatorTests.cs
@@ -0,0 +1,86 @@
+﻿
+
+namespace StringCalculator;
+public class CalculatorTests
+{
+    [Fact]
+    public void EmptyStringReturnsZero()
+    {
+        var calculator = new Calculator();
+
+        var result = calculator.Add("");
+
+        Assert.Equal(0, result);
+    }
+
+    [Theory]
+    [InlineData("3", 3)]
+    public void CanAddASingleNumber(string value, int expected)
+    {
+        var calculator = new Calculator();
+
+        var result = calculator.Add(value);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("3,3", 6)]
+    [InlineData("4,20", 24)]
+    [InlineData("5,30", 35)]
+    [InlineData("6,40", 46)]
+    [InlineData("7,50", 57)]
+    public void CanAddTwoNumbers(string value, int expected)
+    {
+        var calculator = new Calculator();
+
+        var result = calculator.Add(value);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("3,3,3", 9)]
+    [InlineData("4,20,1", 25)]
+    [InlineData("5,30,1", 36)]
+    [InlineData("6,41,3", 50)]
+    [InlineData("1,2,3,4,5,6", 21)]
+    public void CanAddMultipleNumbers(string value, int expected)
+    {
+        var calculator = new Calculator();
+
+        var result = calculator.Add(value);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("1\n2", 3)]
+    [InlineData("4\n20", 24)]
+    [InlineData("5\n30", 35)]
+    [InlineData("6\n41", 47)]
+    [InlineData("1\n9", 10)]
+    public void CanAddNonCommaNumbers(string value, int expected)
+    {
+        var calculator = new Calculator();
+
+        var result = calculator.Add(value);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("1\n2,5", 8)]
+    [InlineData("4\n20,5", 29)]
+    [InlineData("5\n30,5", 40)]
+    [InlineData("6\n41,5", 52)]
+    [InlineData("1\n9,5", 15)]
+    public void CanAddNonCommaAndCommaNumbers(string value, int expected)
+    {
+        var calculator = new Calculator();
+
+        var result = calculator.Add(value);
+
+        Assert.Equal(expected, result);
+    }
+}
